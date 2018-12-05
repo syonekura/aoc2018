fecde70b1be6fcddf3069c2e7221ffefe271d861
Sebastian Yonekura <sebastian.yonekura@cencosud.cl>
2018-12-05 10:52:45 -0300

    complete day 1

--- aoc/day1.py
+++ aoc/day1.py
@@ -1,11 +1,27 @@
+from itertools import accumulate, cycle
 from typing import List
 
 
-def chronal_calibration(input: List[str]):
-    return sum(int(x) for x in input)
+def as_int(text):
+    for x in text:
+        yield int(x)
+
+
+def chronal_calibration(text: List[str]):
+    return sum(as_int(text))
+
+
+def twice_frequency(text):
+    frequencies = {0}
+    for freq in accumulate(cycle(as_int(text))):
+        if freq in frequencies:
+            return freq
+        else:
+            frequencies.add(freq)
 
 
 if __name__ == '__main__':
     with open('../data/day1/chronal_calibration.txt') as file:
         text_input = file.readlines()
-    print(chronal_calibration(text_input))
+    print(f'Day 1 Part 1: {chronal_calibration(text_input)}')
+    print(f'Day 1 Part 2: {twice_frequency(text_input)}')
--- tests/test_day1.py
+++ tests/test_day1.py
@@ -9,3 +9,13 @@ from aoc import day1
 ])
 def chronal_calibration(arg, expected):
     assert day1.chronal_calibration(arg) == expected
+
+
+@pytest.mark.parametrize("arg, expected", [
+    ('+1, -1'.split(','), 0),
+    ('+3, +3, +4, -2, -4'.split(','), 10),
+    ('-6, +3, +8, +5, -6'.split(','), 5),
+    ('+7, +7, -2, -7, -4'.split(','), 14)
+])
+def test_twice_frequency(arg, expected):
+    assert day1.twice_frequency(arg) == expected
