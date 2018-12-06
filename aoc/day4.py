79741f1684ab52aa06abc645a2cfce79a6f74ebe
Sebastian Yonekura <sebastian.yonekura@cencosud.cl>
2018-12-06 11:27:18 -0300

    day 4 wip

--- aoc/day4.py
+++ aoc/day4.py
@@ -1,5 +1,39 @@
+from datetime import datetime, date
+import re
+
+
 def sort_records(shuffled: str):
     return '\n'.join(list(sorted(shuffled.splitlines())))
+
+
+def parse_date(record) -> date:
+    return datetime.strptime(record[0:18], '[%Y-%m-%d %H:%M]').date()
+
+
+def parse_time(record) -> int:
+    record_time = datetime.strptime(record[0:18], '[%Y-%m-%d %H:%M]').time()
+    if record_time.hour > 0:
+        return 0
+    else:
+        return record_time.minute
+
+
+def parse_event(record):
+    if 'begins shift' in record:
+        return 'begins shift'
+    elif 'wakes up' in record:
+        return 'wakes up'
+    elif 'falls asleep' in record:
+        return 'falls asleep'
+    return None
+
+
+def parse_id(record):
+    pattern = re.compile(r'.*\s#(\d+)\s.*')
+    if pattern.match(record):
+        return int(pattern.sub('\\1', record))
+    else:
+        return None
 
 
 def strategy_1(rec):
--- tests/test_day4.py
+++ tests/test_day4.py
@@ -1,6 +1,7 @@
 import pytest
 import random
 from aoc import day4
+from datetime import date
 
 records = """
 [1518-11-01 00:00] Guard #10 begins shift
@@ -35,3 +36,38 @@ def test_sort_records():
     random.shuffle(shuffled)
     shuffled = '\n'.join(shuffled)
     assert day4.sort_records(shuffled) == records
+
+
+@pytest.mark.parametrize("record, expected", [
+    ('[1518-11-01 00:00] Guard #10 begins shift', date(1518, 11, 1)),
+    ('[1518-11-02 00:50] wakes up', date(1518, 11, 2))
+])
+def test_parse_date(record, expected):
+    assert day4.parse_date(record) == expected
+
+
+@pytest.mark.parametrize("record, expected", [
+    ('[1518-11-01 00:05] Guard #10 begins shift', 5),
+    ('[1518-11-02 00:50] wakes up', 50),
+    ('[1518-11-01 23:58] Guard #99 begins shift', 0)
+])
+def test_parse_time(record, expected):
+    assert day4.parse_time(record) == expected
+
+
+@pytest.mark.parametrize("record, expected", [
+    ('[1518-11-01 00:05] Guard #10 begins shift', 'begins shift'),
+    ('[1518-11-02 00:50] wakes up', 'wakes up'),
+    ('[1518-11-03 00:24] falls asleep', 'falls asleep')
+])
+def test_parse_event(record, expected):
+    assert day4.parse_event(record) == expected
+
+
+@pytest.mark.parametrize("record, expected", [
+    ('[1518-11-01 00:05] Guard #10 begins shift', 10),
+    ('[1518-11-02 00:50] wakes up', None),
+    ('[1518-11-03 00:24] falls asleep', None)
+])
+def test_parse_id(record, expected):
+    assert day4.parse_id(record) == expected
