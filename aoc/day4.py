9e1f681486cb74ffef83e1e3d94aa2ce9f4c6dc4
Sebastian Yonekura <sebastian.yonekura@cencosud.cl>
2018-12-07 14:52:59 -0300

    day 4 complete

--- aoc/day4.py
+++ aoc/day4.py
@@ -1,13 +1,21 @@
-from datetime import datetime, date
+from datetime import datetime, date, timedelta
 import re
+import numpy as np
+from itertools import groupby
+from functools import reduce
 
 
 def sort_records(shuffled: str):
-    return '\n'.join(list(sorted(shuffled.splitlines())))
+    return list(sorted(shuffled.splitlines()))
 
 
 def parse_date(record) -> date:
-    return datetime.strptime(record[0:18], '[%Y-%m-%d %H:%M]').date()
+    timestamp = datetime.strptime(record[0:18], '[%Y-%m-%d %H:%M]')
+    if timestamp.time().hour > 0:
+        dt = timestamp.date()
+        return date(dt.year, dt.month, dt.day) + timedelta(days=1)
+    else:
+        return timestamp.date()
 
 
 def parse_time(record) -> int:
@@ -36,5 +44,59 @@ def parse_id(record):
         return None
 
 
+def parse_records(rec):
+    records = sort_records(rec)
+    dates = map(parse_date, records)
+    minutes = map(parse_time, records)
+    events = map(parse_event, records)
+    ids = map(parse_id, records)
+    records = dict()
+    last_id = None
+    for dt, minute, event, gid in zip(dates, minutes, events, ids):
+        if gid is not None:
+            last_id = gid
+
+        if event == 'begins shift':
+            if last_id not in records.keys():
+                records[last_id] = dict()
+            records[last_id][dt] = np.zeros(60, dtype=int)
+        elif event == 'falls asleep':
+            records[last_id][dt][minute] = 1
+        elif event == 'wakes up':
+            asleep = np.where(records[last_id][dt] == 1)[0].max()
+            records[last_id][dt][asleep:minute] = 1
+    return records
+
+
 def strategy_1(rec):
-    return None
+    # Parse records
+    records = parse_records(rec)
+    # Guard that sleeps the most
+    aux = sorted((gid, sum(mins))
+                 for gid, y in records.items()
+                 for _, mins in y.items())
+    sleeper = max(((gid, reduce(lambda x, y: x + y[1], group, 0))
+                   for gid, group
+                   in groupby(aux, key=lambda x: x[0])),
+                  key=lambda x: x[1])
+    # Minute that sleeps more often
+    minute = np.argmax(reduce(lambda x, y: x + y,
+                              records[sleeper[0]].values()))
+    return sleeper[0] * minute
+
+
+def strategy_2(rec):
+    records = parse_records(rec)
+    # Frequency among minutes
+    freq = {(gid, np.argmax(mins), max(mins))
+            for gid, mins in ((gid, reduce(lambda x, y: x+y, value.values()))
+                              for gid, value in records.items())}
+    max_freq = max(freq, key=lambda x: x[2])
+    return max_freq[0] * max_freq[1]
+
+
+if __name__ == '__main__':
+    with open('../data/day4.txt') as file:
+        recs = file.read()
+    print(f'Strategy 1: {strategy_1(recs)}')
+    print(f'Strategy 2: {strategy_2(recs)}')
--- tests/test_day4.py
+++ tests/test_day4.py
@@ -21,7 +21,7 @@ records = """
 [1518-11-05 00:03] Guard #99 begins shift
 [1518-11-05 00:45] falls asleep
 [1518-11-05 00:55] wakes up
-""".rstrip('\n')
+""".rstrip('\n').lstrip('\n')
 
 
 @pytest.mark.parametrize("rec, expected", [
@@ -31,16 +31,24 @@ def test_strategy_1(rec, expected):
     assert day4.strategy_1(rec) == expected
 
 
+@pytest.mark.parametrize("rec, expected", [
+    (records, 4455)
+])
+def test_strategy_2(rec, expected):
+    assert day4.strategy_2(rec) == expected
+
+
 def test_sort_records():
     shuffled = records.splitlines()
     random.shuffle(shuffled)
     shuffled = '\n'.join(shuffled)
-    assert day4.sort_records(shuffled) == records
+    assert day4.sort_records(shuffled) == records.splitlines()
 
 
 @pytest.mark.parametrize("record, expected", [
     ('[1518-11-01 00:00] Guard #10 begins shift', date(1518, 11, 1)),
-    ('[1518-11-02 00:50] wakes up', date(1518, 11, 2))
+    ('[1518-11-02 00:50] wakes up', date(1518, 11, 2)),
+    ('[1518-11-01 23:58] Guard #99 begins shift', date(1518, 11, 2))
 ])
 def test_parse_date(record, expected):
     assert day4.parse_date(record) == expected
